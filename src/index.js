46dec0444668c246754beede9d8e5089bd843a6f
unknown <ivannabelichenko@gmail.com>
2025-06-10 01:43:11 +0300

    1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-const s ="hello world";
-console.log();
+import { initMongoDB } from './db/initMongoConnection.js';
+import { startServer } from './server.js';
 
+const bootstrap = async () => {
+  await initMongoDB();
+  startServer();
+};
+
+bootstrap();
