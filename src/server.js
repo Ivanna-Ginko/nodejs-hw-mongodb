8a1bf249fb251a6bace611cad813e0a28eecb6cc
unknown <ivannabelichenko@gmail.com>
2025-06-14 00:36:19 +0300

    3-2

--- src/server.js
+++ src/server.js
@@ -12,9 +12,9 @@ export const startServer = () => {
   const app = express();
 
   app.use(express.json({
-	type: ['application/json', 'application/vnd.api+json'],
-  limit: '100kb',
-}));
+	  type: ['application/json', 'application/vnd.api+json'],
+    limit: '100kb',
+  }));
   app.use(cors());
 
   app.use(
