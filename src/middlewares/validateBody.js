f38b9be70cf914a6115f10ddabba64b1039df05c
unknown <ivannabelichenko@gmail.com>
2025-06-26 12:32:55 +0300

    5

--- src/middlewares/validateBody.js
+++ src/middlewares/validateBody.js
@@ -1,11 +1,18 @@
+import createHttpError from 'http-errors';
 
 export const validateBody = (schema) => async (req, res, next) => {
- console.log('rrrrrrrrrrrrrrrrrrrr=');
-  let r = await schema.validateAsync(req.body, {
+  try{
+  await schema.validateAsync(req.body, {
     abortEarly: false,
     convert: false,
     allowUnknown: false,
   });
-  console.log('rrrrrrrrrrrrrrrrrrrr=', r);
   next();
+
+  } catch (err) {
+    const error = createHttpError(400, 'Bad Request', {
+      errors: err.details,
+    });
+    next(error);
+  }
 };
